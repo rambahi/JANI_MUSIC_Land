c3e4d425539daa44f20a1bb04ad6df92a00fdaeb
rambahi <pk0031641@gmail.com>
2025-05-02 23:17:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/rambahi/JANI_MUSIC_Land",
+    "https://github.com/rambahi/Jani-Music-Repo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
